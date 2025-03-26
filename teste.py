6577caf6c2e1ba7cb172730e8b6549fca6314a5a
TobiasMaugus <tobiasmaugus705@gmail.com>
2025-03-26 16:26:53 -0300

    ACHO q dijkstra tá certo

--- teste.py
+++ teste.py
@@ -99,6 +99,58 @@ def calcula_graus(vertices, edges, arcs):
     
     return graus_set
 
+def dijkstra(start_node, edges, arcs, end_node=None):
+    distancias = {start_node: 0} 
+    predecessores = {start_node: None}
+    nos_visitados = set()
+    fila_prioridade = {start_node: 0}
+    
+    while fila_prioridade:
+        node = min(fila_prioridade, key=fila_prioridade.get)
+        fila_prioridade.pop(node)
+        nos_visitados.add(node)
+
+        for (u, v), (t_cost) in edges:
+            if u == node and v not in nos_visitados:
+                nova_distancia = distancias[u] + t_cost
+                if v not in distancias or nova_distancia < distancias[v]:
+                    distancias[v] = nova_distancia
+                    predecessores[v] = u
+                    fila_prioridade[v] = nova_distancia
+                    
+            if v == node and u not in nos_visitados:
+                nova_distancia = distancias[v] + t_cost
+                if u not in distancias or nova_distancia < distancias[u]:
+                    distancias[u] = nova_distancia
+                    predecessores[u] = v
+                    fila_prioridade[u] = nova_distancia
+        
+
+        for (u, v), (t_cost) in arcs:
+            if u == node and v not in nos_visitados:
+                nova_distancia = distancias[u] + t_cost
+                if v not in distancias or nova_distancia < distancias[v]:
+                    distancias[v] = nova_distancia
+                    predecessores[v] = u
+                    fila_prioridade[v] = nova_distancia
+                    
+
+    distancias_ordenadas = {key: distancias[key] for key in sorted(distancias)}
+
+    if end_node is not None:
+        caminho = []
+        current_node = end_node
+  
+        while current_node != start_node:
+            caminho.append(current_node)
+            current_node = predecessores.get(current_node)
+        
+        caminho.append(start_node)
+        caminho.reverse()
+        return distancias_ordenadas, tuple(caminho)
+
+
+    return distancias_ordenadas
 
 
 
@@ -107,15 +159,17 @@ file_path = "teste.dat"
 vertices, edges, arcs, required_vertices, required_edges, required_arcs = count_metrics(file_path)
 graus = calcula_graus(vertices, edges, arcs)
 
-
+start_node = list(vertices)[0]
+last_node = list(vertices)[1]
+distancias, caminho = dijkstra(start_node, edges, arcs, last_node)
 # print(f" vértices: {(vertices)}")
 # print(f"Quantidade de arestas: {len(edges)}")
 # print(f"Quantidade de arcos: {len(arcs)}")
 # print(f"Quantidade de vértices requeridos: {len(required_vertices)}")
 # print(f"Quantidade de arestas requeridas: {len(required_edges)}")
 # print(f"Quantidade de arcos requeridos: {len(required_arcs)}")
-print(graus)
-
+print (distancias)
+print (caminho)
 
 #TODO
 # 1. Quantidade de vértices; OK
